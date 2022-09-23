5f52cdd3ae4c60078295eb3286e5277fefe80279
florian.sabonchi <sabonchi@posteo.de>
2022-09-23 17:08:39 +0200

    fixed os.getenv

--- app/db.py
+++ app/db.py
@@ -5,10 +5,11 @@ import os
 from sqlalchemy.ext.declarative import declarative_base
 
 user = os.getenv('POSTGRES_USER')
-password = os.environ.get('POSTGRES_PASSWORD')
-database = os.environ.get('POSTGRES_DB')
+password = os.getenv('POSTGRES_PASSWORD')
+database = os.getenv('POSTGRES_DB')
+host = os.getenv("POSTGRES_HOST")
 
 
-engine = create_engine(f'postgresql://{user}:{password}@localhost:5432/{database}', echo=True)
+engine = create_engine(f'postgresql://{user}:{password}@{host}:5432/{database}', echo=True)
 
 Base = declarative_base()

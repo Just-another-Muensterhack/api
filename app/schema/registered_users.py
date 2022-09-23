9807d3e129eb76fe1bb552058b2f10da59a313c9
florian.sabonchi <sabonchi@posteo.de>
2022-09-23 15:50:17 +0200

    first draft for database models

---
+++ app/schema/registered_users.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, String, Enum
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+import enum
+
+from ..db import Base
+
+
+class Role(enum.Enum):
+    admin = 0
+    support = 1
+    user = 2
+
+
+class RegisteredUsers(Base):
+    __tablename__ = "registered_users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    phone_number = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    role = Column(Enum(Role), default=Role.user)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+    hashed_password = Column(String, nullable=True)

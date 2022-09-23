a788dd929973b5d277b33bda8817d668e9c8315e
florian.sabonchi <sabonchi@posteo.de>
2022-09-23 21:36:01 +0200

    fixed pipeline

--- app/schema/user.py
+++ app/schema/user.py
@@ -27,8 +27,10 @@ class User(Model):
 
     @staticmethod
     def create():
-        session.add(User(id=uuid.uuid4()
-                         , created_at=datetime.datetime.utcnow()))
+        session.add(User(
+            id=uuid.uuid4(),
+            created_at=datetime.datetime.utcnow()
+        ))
         session.commit()
 
     @staticmethod
